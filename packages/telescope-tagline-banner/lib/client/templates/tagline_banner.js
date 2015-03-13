6010ec3f4e61aa5a3e0a2186aa17d1e1982e8ca5
Rachel Berry <crb@berrycs.com>
2015-03-13 02:15:24 -0700

    Fix title/tagline on banner

--- packages/telescope-tagline-banner/lib/client/templates/tagline_banner.js
+++ packages/telescope-tagline-banner/lib/client/templates/tagline_banner.js
@@ -1,6 +1,6 @@
 Template[getTemplate('taglineBanner')].helpers({
   showTaglineBanner: function () {
-    return Router.current().location.get().path == '/' && !!getSetting('tagline') && !!getSetting('showTaglineBanner');
+    return Router.current().location.get().path == '/' && !!getSetting('title') && !!getSetting('tagline') && !!getSetting('showTaglineBanner');
   }
 });
 
